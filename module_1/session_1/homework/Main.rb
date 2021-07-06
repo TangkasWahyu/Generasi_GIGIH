050a9f3f9fa814f5d95b7c53fee71c626f6a9f5f
Yudo Kartiko <yudoworks@gmail.com>
2021-07-06 13:30:00 +0700

    Add deflect chance

--- module_1/session_1/homework/Main.rb
+++ module_1/session_1/homework/Main.rb
@@ -1,13 +1,16 @@
 require_relative 'Warrior'
 
-jin = Warrior.new("Jin Sakai", 50, 100)
+jin = Warrior.new("Jin Sakai", 50, 100, 0.8)
 khotun = Warrior.new("Khotun Khan", 50, 500)
-puts jin
+puts jin 
 puts khotun
 
-while jin.isAlive? && khotun.isAlive?
+while true
     jin.attack(khotun)
+    break unless khotun.isAlive? 
+
     khotun.attack(jin)
+    break unless jin.isAlive?
 end
 
 puts "#{jin.name} is dead" unless jin.isAlive? 
--- module_1/session_1/homework/Warrior.rb
+++ module_1/session_1/homework/Warrior.rb
@@ -1,14 +1,19 @@
 class Warrior
     attr_reader :name
 
-    def initialize(name, attackDamage, hitpoints)
+    def initialize(name, attackDamage, hitpoints, deflactChance = 0)
         @name = name
         @hitpoints = hitpoints
         @attackDmg = attackDamage
+        @deflactChance = deflactChance
     end
 
     def to_s
-        "#{@name} has #{@hitpoints} hitpoint and #{@attackDmg} attack damage"
+        "#{@name} has #{@hitpoints} hitpoint and #{@attackDmg} attack damage \n\n"
+    end
+
+    def isAlive?
+        @hitpoints > 0
     end
 
     def attack(other)
@@ -18,10 +23,16 @@ class Warrior
     end
 
     def attacked(attackDamage)
+        if isDeflect?
+            attackDamage = 0
+            puts "#{@name} deflect the attack"
+        end
+
         @hitpoints -= attackDamage
     end
 
-    def isAlive?
-        @hitpoints > 0
+    def isDeflect?
+        randomNumber = rand(@deflactChance * 10).to_int
+        randomNumber.nonzero?
     end
 end
