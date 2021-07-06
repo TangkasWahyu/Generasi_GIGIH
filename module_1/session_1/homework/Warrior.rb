8814f1e7e04121d0fd8e8cfaf74ec2e5dd5ad7c0
Yudo Kartiko <yudoworks@gmail.com>
2021-07-06 18:45:35 +0700

    change to snake case

--- module_1/session_1/homework/Warrior.rb
+++ module_1/session_1/homework/Warrior.rb
@@ -1,15 +1,15 @@
 class Warrior
     attr_reader :name
 
-    def initialize(name, attackDamage, hitpoints, deflectChance = 0)
+    def initialize(name, attack_damage, hitpoints, deflect_chance = 0)
         @name = name
         @hitpoints = hitpoints
-        @attackDmg = attackDamage
-        @deflectChance = deflectChance
+        @attack_damage = attack_damage
+        @deflect_chance = deflect_chance
     end
 
     def to_s
-        "#{@name} has #{@hitpoints} hitpoint and #{@attackDmg} attack damage \n\n"
+        "#{@name} has #{@hitpoints} hitpoint and #{@attack_damage} attack damage \n\n"
     end
 
     def isAlive?
@@ -17,26 +17,26 @@ class Warrior
     end
 
     def attack(other)
-        puts "#{@name} attacks #{other.name} with #{@attackDmg}"
-        other.attacked(@attackDmg)
+        puts "#{@name} attacks #{other.name} with #{@attack_damage}"
+        other.attacked(@attack_damage)
         puts other
     end
 
-    def attacked(attackDamage)
+    def attacked(attack_damage)
         if isDeflect?
-            attackDamage = 0
+            attack_damage = 0
             puts "#{@name} deflect the attack"
         end
 
-        @hitpoints -= attackDamage
+        @hitpoints -= attack_damage
     end
 
     def isDeflect?
-        return true if @deflectChance == 1
-        return false if @deflectChance == 0
+        return true if @deflect_chance == 1
+        return false if @deflect_chance == 0
 
-        hittedChanceRatioNumber = 100 / (100 - @deflectChance*100)
-        randomNumber = rand(hittedChanceRatioNumber) + 1
-        randomNumber == hittedChanceRatioNumber
+        hitted_chance_ratio_number = 100 / (100 - @deflect_chance*100)
+        random_number = rand(hitted_chance_ratio_number) + 1
+        random_number == hitted_chance_ratio_number
     end
 end
