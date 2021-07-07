1579f73091e565264c2f04570f0d7b33765878bf
Yudo Kartiko <yudoworks@gmail.com>
2021-07-08 01:31:50 +0700

    fix typo

--- module_1/session_2/exercise_2/Swordman.rb
+++ module_1/session_2/exercise_2/Swordman.rb
@@ -2,7 +2,7 @@ require_relative 'Mongol'
 
 class Swordman < Mongol
     def attack(other_warrior)
-        puts "#{@name} slash #{other_warrior.name} with #{@attack_damage} damage \n"
+        puts "#{@name} slashes #{other_warrior.name} with #{@attack_damage} damage \n"
         super(other_warrior)
     end
 end
